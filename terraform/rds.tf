7241beb1c51f04b51a9e9e242bc0d9c5f33b11e7
sabeel28x <138629101+sabeel28x@users.noreply.github.com>
2026-03-06 10:42:09 +0530

    Update rds.tf

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -1,9 +1,12 @@
-resource "aws_db_instance" "db" {
-  allocated_storage    = 20
-  engine               = "postgres"
-  instance_class       = "db.t3.micro"
-  name                 = "ecostream"
-  username             = "admin"
-  password             = "changeme"
-  skip_final_snapshot  = true
+resource "aws_db_instance" "postgres" {
+
+  allocated_storage = 20
+  engine            = "postgres"
+
+  instance_class = "db.t3.micro"
+
+  username = "admin"
+  password = "StrongPassword123"
+
+  skip_final_snapshot = true
 }
