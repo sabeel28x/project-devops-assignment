ff8453945b3f859012ccf2abeebc7c9a29adc951
sabeel28x <138629101+sabeel28x@users.noreply.github.com>
2026-03-06 10:41:27 +0530

    Update ecs.tf

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -1,3 +1,36 @@
 resource "aws_ecs_cluster" "main" {
   name = "ecostream-cluster"
 }
+
+resource "aws_ecs_task_definition" "app" {
+  family                   = "ecostream-task"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+
+  cpu    = "256"
+  memory = "512"
+
+  container_definitions = jsonencode([
+    {
+      name  = "ecostream-container"
+      image = "${aws_ecr_repository.app_repo.repository_url}:latest"
+
+      portMappings = [
+        {
+          containerPort = 5000
+        }
+      ]
+    }
+  ])
+}
+
+resource "aws_ecs_service" "app_service" {
+
+  name            = "ecostream-service"
+  cluster         = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.app.arn
+
+  desired_count = 2
+  launch_type   = "FARGATE"
+
+}
